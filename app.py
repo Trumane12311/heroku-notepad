a91e38a24f1a0e0632cff04c0397b4ba76ba6f56
Trumane12311 <Trumane12311@gmail.com>
2021-07-05 16:29:36 -0400

    Added MONGO connection string for cloud

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect
+from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 import ScrapedRandomRollerCoaster
 
-app = Flask(__name__)
+load_dotenv()
 
+app = Flask(__name__)
+app.config['MONGODB_URI'] = environ.get('MONGODB_URI')
+
+#database set-up
 mongo = PyMongo(app, uri="mongodb://localhost:27017/randomcoasterDB")
 
 @app.route("/")
@@ -17,6 +22,24 @@ def scrape():
     mongo.db.random_coaster.update({}, random_data, upsert=True)
     
     return redirect("/")
+
+@app.route('/api/coasters/mongo')
+def coasters_mongo():
+    coasters = mongo.rc_db.coasters.find()
+    data = []
+
+    for coaster in coasters:
+        data.append({
+          '_id': str(coaster['_id']),
+          'Roller Coaster': str(coaster['Roller Coaster']),
+          'Amusement Park': str(coaster['Amusement Park']),
+          'Type': str(coaster['Type']),
+          'Design': str(coaster['Design']),
+          'Opened': datetime(coaster['Opened'])
+        })
+
+    return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
