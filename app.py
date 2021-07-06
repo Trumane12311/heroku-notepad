6fa4614f5d09fbfea89fe6239257fb4e9e35aa60
Trumane12311 <Trumane12311@gmail.com>
2021-07-06 10:18:50 -0400

    Reversing App to resolve issues

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
-from flask import Flask, render_template, redirect
-from dotenv import load_dotenv
-from os import environ
+from flask import Flask, jsonify, render_template, redirect
 from flask_pymongo import PyMongo
-import ScrapedRandomRollerCoaster
-
-load_dotenv()
+#from os import environ
+#from dotenv import find_dotenv, load_dotenv
+#import ScrapedRandomRollerCoaster
 
 app = Flask(__name__)
-app.config['MONGODB_URI'] = environ.get('MONGODB_URI')
+app.config['MONGO_URI'] = environ.get(
+    'MONGO_URI')
 
 #database set-up
-mongo = PyMongo(app, uri=
+mongo = PyMongo(app)
 
 @app.route("/")
 def home():
-    random_coaster = mongo.db.random_coaster.find_one()
-    return render_template("index.html", random_data=random_coaster)
+    return Welcome to Our Roller Coaster Database API!
 
-@app.route("/scrape")
-def scrape():
-    random_data = ScrapedRandomRollerCoaster.scrape()
-    mongo.db.random_coaster.update({}, random_data, upsert=True)
+#@app.route("/")
+#def home():
+    #random_coaster = mongo.db.random_coaster.find_one()
+    #return render_template("index.html", random_data=random_coaster)
+
+#@app.route("/scrape")
+#def scrape():
+    #random_data = ScrapedRandomRollerCoaster.scrape()
+    #mongo.db.random_coaster.update({}, random_data, upsert=True)
 
 @app.route('/api/coasters/mongo')
 def coasters_mongo():
-    coasters = mongo.db.rc_db.find()
+    coasters = mongo.db.rc_DB.find()
     data = []
 
     for coaster in coasters:
