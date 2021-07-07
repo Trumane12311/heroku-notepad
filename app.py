3ba0bcbabe922ef53a5a1c8a255679a2e35a8c9f
Trumane12311 <Trumane12311@gmail.com>
2021-07-07 14:06:25 -0400

    Update

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, render_template, redirect, request
 from flask_pymongo import PyMongo
 from os import environ
 from dotenv import find_dotenv, load_dotenv
-from selenium_chrome import Chrome
-import ScrapedRandomRollerCoaster
+#from selenium_chrome import Chrome
+#import ScrapedRandomRollerCoaster
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = environ.get('MONGODB_URI', 'mongodb://localhost:27017/rc_DB')
